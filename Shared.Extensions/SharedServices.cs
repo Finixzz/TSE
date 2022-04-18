1a84c7be29a20326e402b2feaf54715aeff33541
Ahmet Durmic <ahmetdurmic97@outlook.com>
2022-04-18 17:31:50 +0200

    Renamed method to CheckIsConfigurationEligible in SharedServices

--- Shared.Extensions/SharedServices.cs
+++ Shared.Extensions/SharedServices.cs
@@ -16,7 +16,7 @@ public class ConfigurationService : IConfigurationService
 {
     public AuthConfiguration GetAuthConfiguration(AuthenticationType authType, string configuration)
     {
-        CheckIsConfigurationValid(configuration);
+        CheckIsConfigurationEligible(configuration);
 
         return new
         {
@@ -31,7 +31,7 @@ public class ConfigurationService : IConfigurationService
         if (provider.GetType() != typeof(RelationalDatabaseProvider) && provider.GetType() != typeof(NonRelationalDatabaseProvider))
             throw new ArgumentException(nameof(provider));
 
-        CheckIsConfigurationValid(configuration);
+        CheckIsConfigurationEligible(configuration);
 
         return new
         {
@@ -40,7 +40,7 @@ public class ConfigurationService : IConfigurationService
         }.ActLike<DatabaseConfiguration<TProvider>>();
     }
 
-    private void CheckIsConfigurationValid(string configuration)
+    private void CheckIsConfigurationEligible(string configuration)
     {
         if (string.IsNullOrEmpty(configuration) || string.IsNullOrWhiteSpace(configuration))
             throw new ArgumentException(nameof(configuration));
